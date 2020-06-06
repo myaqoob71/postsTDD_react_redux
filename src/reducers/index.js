3b489c2a3843d10162f2891a41297942b8afeea7
yaqoom2 <yaqoom2@medtronic.com>
2020-06-07 00:17:17 +0530

    Posts reducer with tests

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
-import success from './successReducer';
+import posts from './postsReducer';
 
 export default combineReducers({
-    success
+    posts
 });
